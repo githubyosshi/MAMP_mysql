2acf5ba378c0a4f34435302ce173e61813ec4cb3
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-19 15:47:46 +0900

    Get value from database commit

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
   // echo "Hello Word!";
   // mysqli_connect("localhosts","root","root");
-  mysqli_connect("localhost","root","root");
+  // mysqli_connect("localhost","root","root");
+  $link = mysqli_connect("localhost","root","root","memberapp");
   // サーバー名、データベースユーザー名、パスワード
+
   if(mysqli_connect_error()){
     echo "データベースへの接続に失敗しました。";
-  } else  {
-    echo "データベースへの接続に成功しました。";
+  //   echo "データベースへの接続に失敗しました。";
+  // } else  {
+  //   echo "データベースへの接続に成功しました。";
+    die("データベースへの接続に失敗しました。");
   }
+  $query = "SELECT * FROM users";
+  // if(mysqli_query($link,$query)){
+  if($result = mysqli_query($link,$query)){
 ?>
